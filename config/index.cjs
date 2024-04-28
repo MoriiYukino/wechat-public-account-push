0e5975250497d532cb2d904e6d93199260cf4f87
龙墨文 <84225567+MoriiYukino@users.noreply.github.com>
2024-04-28 21:11:27 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -59,7 +59,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU29803Tdx7OTBDRP866Kb5FvzajIHO3fhfwvDV7',
+      id: '',
     }
   ],
 
