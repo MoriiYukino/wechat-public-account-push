b160a28871e1da5d3fbca203141ca511453db43f
龙墨文 <84225567+MoriiYukino@users.noreply.github.com>
2024-04-28 20:02:46 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -15,8 +15,8 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '河北',
+  CITY: '北京',
 
   USERS: [
     {
@@ -30,23 +30,23 @@ const USER_CONFIG = {
       horoscopeDate: '10-12',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
+        // {
+        //   type: '*生日', name: '释予', year: '2002', date: '09-09',
+        // },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '释予', year: '2002', date: '10-12',
         },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
-        },
+        // {
+        //   type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+        // },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2024-04-23' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        // { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
   ],
