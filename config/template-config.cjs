7be075923d512d6501d353a7e283487efbe80d95
龙墨文 <84225567+MoriiYukino@users.noreply.github.com>
2024-04-28 21:15:00 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -10,19 +10,20 @@ const TEMPLATE_CONFIG = [
       
       {{birthday_message.DATA}}
        
-      城市：{{city.DATA}}   天气：{{weather.DATA}}
+      城市：{{city.DATA}}  天气：{{weather.DATA}}  气温(最高/最低):{{max_temperature.DATA}} / {{min_temperature.DATA}}   
       
-      气温(最高/最低):{{max_temperature.DATA}} / {{min_temperature.DATA}}   风向: {{wind_direction.DATA}}   风级: {{wind_scale.DATA}}
-
-      空气质量指数: {{aqi.DATA}}
+      风向: {{wind_direction.DATA}}   风级: {{wind_scale.DATA}}  空气质量指数: {{aqi.DATA}}
       
-      {{ganmao.DATA}}，{{notice.DATA}}
+      {{ganmao.DATA}}
+      
+      {{notice.DATA}}
 
       距离下个休息日≡ω≡：{{holidaytts.DATA}}
       
       {{comprehensive_horoscope.DATA}}
 
       每日一句：
+      
       {{note_en.DATA}}
       
       {{note_ch.DATA}}
